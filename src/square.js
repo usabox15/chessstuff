c6ad02a38870409363063650ec47f7f9fb684546
Yegor Bitensky <yegorbitensky@gmail.com>
2020-04-20 00:54:37 +0300

    improve board module documentation

--- src/square.js
+++ src/square.js
@@ -4,8 +4,6 @@ var relations = require('./relations');
 class SquareName {
     /*
     Human readable chess board square name.
-    Create param:
-      - name [string] (include two characters - symbol and number, for example 'a1').
       _ _ _ _ _ _ _ _
     8|_|_|_|_|_|_|_|_|
     7|_|_|_|_|_|_|_|_|
@@ -16,19 +14,27 @@ class SquareName {
     2|_|_|_|_|_|_|_|_|
     1|_|_|_|_|_|_|_|_|
       a b c d e f g h
+
+    Example:
+        new SquareName('a1');
     */
 
-    static symbols = ["a", "b", "c", "d", "e", "f", "g", "h"];
-    static numbers = ["1", "2", "3", "4", "5", "6", "7", "8"];
+    static symbols = ["a", "b", "c", "d", "e", "f", "g", "h"]; // possible symbols
+    static numbers = ["1", "2", "3", "4", "5", "6", "7", "8"]; // possible numbers
 
     constructor(name) {
+        /*
+        Params:
+            name {string} - two characters square name (includes symbol and number).
+        */
+
         let symbol = name[0];
         let number = name[1];
         if (!SquareName.symbols.includes(symbol)) {
-            throw Error(`Wrong symbol (${symbol}) passed`);
+            throw Error(`Wrong symbol (${symbol}) passed. Try one of ${SquareName.symbols}`);
         }
         if (!SquareName.numbers.includes(number)) {
-            throw Error(`Wrong number (${number}) passed`);
+            throw Error(`Wrong number (${number}) passed. Try one of ${SquareName.numbers}`);
         }
         this._symbol = symbol;
         this._number = number;
@@ -52,8 +58,6 @@ class SquareName {
 class SquareCoordinates {
     /*
     Chess board square coordinates.
-    Create param:
-      - coordinates [Array] (include two numbers - square coordinates from 0 to 7, for example [0, 0]).
       _ _ _ _ _ _ _ _
     7|_|_|_|_|_|_|_|_|
     6|_|_|_|_|_|_|_|_|
@@ -64,26 +68,47 @@ class SquareCoordinates {
     1|_|_|_|_|_|_|_|_|
     0|_|_|_|_|_|_|_|_|
       0 1 2 3 4 5 6 7
+
+    Example:
+        new SquareCoordinates([0, 0]);
     */
 
-    static numbers = [0, 1, 2, 3, 4, 5, 6, 7];
+    static numbers = [0, 1, 2, 3, 4, 5, 6, 7]; // possible numbers
 
     static correctCoordinate(coordinate) {
+        /*
+        Check whether coordinate is correct or not.
+        Params:
+            coordinate {number}.
+        */
+
         return SquareCoordinates.numbers.includes(coordinate);
     }
 
     static correctCoordinates(x, y) {
+        /*
+        Check whether coordinates (x and y) are correct or not.
+        Params:
+            x {number};
+            y {number}.
+        */
+
         return SquareCoordinates.correctCoordinate(x) && SquareCoordinates.correctCoordinate(y);
     }
 
     constructor(coordinates) {
+        /*
+        Params:
+            coordinates {Array} - two numbers from 0 to 7.
+        */
+
         let x = coordinates[0];
         let y = coordinates[1];
         if (!SquareCoordinates.correctCoordinate(x)) {
-            throw Error(`Wrong x value (${x}) passed`);
+            throw Error(`Wrong x value (${x}) passed. Try one of ${SquareCoordinates.numbers}`);
         }
         if (!SquareCoordinates.correctCoordinate(y)) {
-            throw Error(`Wrong y value (${y}) passed`);
+            throw Error(`Wrong y value (${y}) passed. Try one of ${SquareCoordinates.numbers}`);
         }
         this._x = x;
         this._y = y;
@@ -105,13 +130,14 @@ class SquareCoordinates {
 
 
 class SquareOnEdge {
-    /*
-    Chess board square location on edge.
-    Create param:
-      - coordinates [SquareCoordinates].
-    */
+    // Chess board square location on edge.
 
     constructor(coordinates) {
+        /*
+        Params:
+            coordinates {SquareCoordinates}.
+        */
+
         this._up = false;
         this._right = false;
         this._down = false;
@@ -147,14 +173,15 @@ class SquareOnEdge {
 
 
 class SquaresLine {
-    /*
-    Line between two chess board squares.
-    Create param:
-      - startSquare [Square];
-      - endSquare [Square].
-    */
+    // Line between two chess board squares.
 
     constructor(startSquare, endSquare) {
+        /*
+        Params:
+            startSquare {Square};
+            endSquare {Square}.
+        */
+
         this._startSquare = startSquare;
         this._endSquare = endSquare;
         this._dx = Math.abs(startSquare.coordinates.x - endSquare.coordinates.x);
@@ -166,7 +193,8 @@ class SquaresLine {
     }
 
     _checkOnTheSameLine() {
-        // throw error if squares aren't located on the same line
+        // Throw error if squares aren't located on the same line
+
         if (this._dx != this._dy && this._dx != 0 && this._dy != 0) {
             throw Error(`
                 Squares ${this._startSquare.name.value} and ${this._endSquare.name.value}
@@ -176,7 +204,8 @@ class SquaresLine {
     }
 
     _getDirection() {
-        // get direction of distance between start square and end square
+        // Get direction of distance between start square and end square
+
         this._checkOnTheSameLine();
         for (let i of ['x', 'y']) {
             if (this._startSquare.coordinates[i] > this._endSquare.coordinates[i]) {
@@ -189,7 +218,8 @@ class SquaresLine {
     }
 
     _getBetweenSquaresData() {
-        // get data of squares between start square and end square
+        // Get names and count of squares between start square and end square
+
         this._getDirection();
         let distance = Math.max(this._dx, this._dy);
         for (let i = 1; i <= distance - 1; i++) {
@@ -203,6 +233,13 @@ class SquaresLine {
     }
 
     betweenSquaresNames(includeStart=false, includeEnd=false) {
+        /*
+        Get squares names between start square and end square
+        Params:
+            includeStart {boolean} - whether include start square name or not;
+            includeEnd {boolean} - whether include end square name or not.
+        */
+
         let additional = [];
         if (includeStart) {
             additional.push(this._startSquare.name.value);
@@ -214,12 +251,21 @@ class SquaresLine {
     }
 
     betweenSquaresCount(includeStart=false, includeEnd=false) {
+        /*
+        Get squares count between start square and end square
+        Params:
+            includeStart {boolean} - whether include start square or not;
+            includeEnd {boolean} - whether include end square or not.
+        */
+
         return this._betweenSquaresCount + (includeStart ? 1 : 0) + (includeEnd ? 1 : 0);
     }
 }
 
 
 class SquareNeighbors {
+    // Chess board square neighbors squares.
+
     static UP_LEFT = 'upLeft';
     static UP = 'up';
     static UP_RIGHT = 'upRight';
@@ -251,6 +297,11 @@ class SquareNeighbors {
     };
 
     constructor(square) {
+        /*
+        Params:
+            square {Square}.
+        */
+
         this._square = square;
     }
 
@@ -287,6 +338,12 @@ class SquareNeighbors {
     }
 
     _getSquare(kind) {
+        /*
+        Get neighbor.
+        Params:
+            kind {string} - direction name.
+        */
+
         if (!this._square.board || !this.#validate[kind](this._square)) return null;
         let x = this._square.coordinates.x + this.#delta[kind].x;
         let y = this._square.coordinates.y + this.#delta[kind].y;
@@ -297,38 +354,43 @@ class SquareNeighbors {
 
 
 class Square {
-    /*
-    Chess board square.
-    There are create params:
-      - identifier [string or Array] (SquareName or SquareCoordinates create param);
-      - board [Board Object] (link to the Board).
-    To create instance you need to pass one of this params.
-    */
+    // Chess board square.
 
     static symbolToNumber = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7};
     static numberToSymbol = {0: "a", 1: "b", 2: "c", 3: "d", 4: "e", 5: "f", 6: "g", 7: "h"};
 
     static coordinatesToName(x, y) {
+        /*
+        Convert square coordinates to square name.
+        Params:
+            x {number};
+            y {number}.
+        */
+
         if (!SquareCoordinates.correctCoordinates(x, y)) {
             throw Error(`Incorrect square coordinates (x - ${x}, y - ${y}).`);
         }
         return Square.numberToSymbol[x] + (y + 1);
     }
 
-    constructor(identifier=null, board=null) {
-        if (!identifier) {
-            throw Error("To create Square instance you need to pass identifier param (name or coordinates)");
-        }
+    constructor(identifier, board=null) {
+        /*
+        Params:
+            identifier {string or Array} - SquareName or SquareCoordinates create param;
+            board {Board} - link to the Board (optional).
+        */
 
         if (typeof(identifier) === 'string') {
             this._name = new SquareName(identifier);
-            this._coordinates = new SquareCoordinates([Square.symbolToNumber[this._name.symbol], +(this._name.number - 1)]);
-        }
-        else if (Array.isArray(identifier)) {
+            let x = Square.symbolToNumber[this._name.symbol];
+            let y = parseInt(this._name.number) - 1;
+            this._coordinates = new SquareCoordinates([x, y]);
+        } else if (Array.isArray(identifier)) {
             this._coordinates = new SquareCoordinates(identifier);
-            this._name = new SquareName(Square.numberToSymbol[this._coordinates.x] + (this._coordinates.y + 1));
-        }
-        else {
+            let symbol = Square.numberToSymbol[this._coordinates.x];
+            let number = this._coordinates.y + 1;
+            this._name = new SquareName(`${symbol}${number}`);
+        } else {
             throw Error("Wrong identifier passed. It need to be SquareName or SquareCoordinates create param.");
         }
 
@@ -361,16 +423,21 @@ class Square {
     }
 
     _getIsLight() {
+        // Check whether square is light or not.
+
         let xIsEven = this.coordinates.x % 2 == 0;
         let yIsEven = this.coordinates.y % 2 == 0;
         return xIsEven && !yIsEven || !xIsEven && yIsEven;
     }
 
-    _fireBoardRefresh(refresh) {
-        if (this.board && refresh) this.board.refreshAllSquares();
-    }
-
     placePiece(piece, refresh=true) {
+        /*
+        Place piece to square.
+        Params:
+            piece {Piece or its subclass};
+            refresh {boolean} - whether need to refresh board after piece has been placed or not.
+        */
+
         if (this.board && !piece.canBeReplacedTo(this)) {
             let positionIsSetted = (
                 this.board.positionIsSetted
@@ -385,31 +452,71 @@ class Square {
             }
         }
         this._piece = piece;
-        this._fireBoardRefresh(refresh);
+        if (refresh && this.board) this.board.refreshAllSquares();
     }
 
     removePiece(refresh=true) {
+        /*
+        Remove piece from square.
+        Params:
+            refresh {boolean} - whether need to refresh board after piece has been placed or not.
+        */
+
         this._piece = null;
-        this._fireBoardRefresh(refresh);
+        if (refresh && this.board) this.board.refreshAllSquares();
     }
 
     theSame(otherSquare) {
+        /*
+        Check square is the same with other square.
+        Params:
+            otherSquare {Square}.
+        */
+
         return this.name.value === otherSquare.name.value;
     }
 
     onVertical(vertical) {
+        /*
+        Check square is placed on particular vertical.
+        Params:
+            vertical {string}.
+        */
+
         return this.name.symbol === vertical;
     }
 
     onRank(rank) {
+        /*
+        Check square is placed on particular rank.
+        Params:
+            rank {number or string}.
+        */
+
         return this.name.number == rank;
     }
 
     getBetweenSquaresNames(otherSquare, includeThisSquare=false, includeOtherSquare=false) {
+        /*
+        Get squares names between this square and other square
+        Params:
+            otherSquare {Square};
+            includeThisSquare {boolean};
+            includeOtherSquare {boolean}.
+        */
+
         return new SquaresLine(this, otherSquare).betweenSquaresNames(includeThisSquare, includeOtherSquare);
     }
 
     getBetweenSquaresCount(otherSquare, includeThisSquare=false, includeOtherSquare=false) {
+        /*
+        Get squares count between this square and other square
+        Params:
+            otherSquare {Square};
+            includeThisSquare {boolean};
+            includeOtherSquare {boolean}.
+        */
+
         return new SquaresLine(this, otherSquare).betweenSquaresCount(includeThisSquare, includeOtherSquare);
     }
 }
