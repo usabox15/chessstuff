10eda2c1272fad0414de585532acc813793d2aa7
Yegor Bitensky <yegorbitensky@gmail.com>
2022-01-03 23:54:42 +0300

    Remove jquery from example

--- examples/example.js
+++ examples/example.js
@@ -3,182 +3,214 @@ const MARKED = 'marked';
 
 const SYMBOLS = {
     [chessstuff.Piece.WHITE]: {
-        [chessstuff.Piece.PAWN]: '&#9817;',
-        [chessstuff.Piece.KNIGHT]: '&#9816;',
-        [chessstuff.Piece.BISHOP]: '&#9815;',
-        [chessstuff.Piece.ROOK]: '&#9814;',
-        [chessstuff.Piece.QUEEN]: '&#9813;',
-        [chessstuff.Piece.KING]: '&#9812;',
+        [chessstuff.Piece.PAWN]: '♙',
+        [chessstuff.Piece.KNIGHT]: '♘',
+        [chessstuff.Piece.BISHOP]: '♗',
+        [chessstuff.Piece.ROOK]: '♖',
+        [chessstuff.Piece.QUEEN]: '♕',
+        [chessstuff.Piece.KING]: '♔',
     },
     [chessstuff.Piece.BLACK]: {
-        [chessstuff.Piece.PAWN]: '&#9823;',
-        [chessstuff.Piece.KNIGHT]: '&#9822;',
-        [chessstuff.Piece.BISHOP]: '&#9821;',
-        [chessstuff.Piece.ROOK]: '&#9820;',
-        [chessstuff.Piece.QUEEN]: '&#9819;',
-        [chessstuff.Piece.KING]: '&#9818;',
+        [chessstuff.Piece.PAWN]: '♟',
+        [chessstuff.Piece.KNIGHT]: '♞',
+        [chessstuff.Piece.BISHOP]: '♝',
+        [chessstuff.Piece.ROOK]: '♜',
+        [chessstuff.Piece.QUEEN]: '♛',
+        [chessstuff.Piece.KING]: '♚',
     },
 };
 
 
-var globalActions = Object.fromEntries(
-  chessstuff.Relation.ALL_KINDS.map(k => [k, true])
-);
-var board = null;
-var aimedSquare = null;
+var GLOBAL = {
+  actions: Object.fromEntries(
+    chessstuff.Relation.ALL_KINDS.map(k => [k, true])
+  ),
+  aimedSquare: null,
+  board: null,
+};
 
 
 function markingSquares(piece, action, aimed=false) {
-    if (!globalActions[action] || !piece.squares[action]) return;
+  if (!GLOBAL.actions[action] || !piece.squares[action]) return;
 
-    let classes = [action, MARKED];
-    if (aimed) {
-      classes.push(AIM);
-    }
+  let classes = [action, MARKED];
+  if (aimed) {
+    classes.push(AIM);
+  }
 
-    for (let sqr of piece.squares[action]) {
-        $(`.square[x=${sqr.name.symbol}][y=${sqr.name.number}]`)
-        .addClass(classes);
-    }
+  for (let sqr of piece.squares[action]) {
+    let x = sqr.name.symbol;
+    let y = sqr.name.number;
+    let square = document.querySelector(`.square[x="${x}"][y="${y}"]`);
+    square.classList.add(...classes);
+  }
 }
+
 
 function markingAbilitySquares(piece, aimed=false) {
-    for (let action of chessstuff.Relation.ALL_KINDS){
-        markingSquares(piece, action, aimed);
-    }
+  for (let action of chessstuff.Relation.ALL_KINDS) {
+    markingSquares(piece, action, aimed);
+  }
 }
+
 
 function unMarkingSquares() {
-    $(".square").removeClass([AIM, MARKED, ...chessstuff.Relation.ALL_KINDS]);
+  let squares = document.querySelectorAll('.square');
+  for (let square of squares) {
+    square.classList.remove(AIM, MARKED, ...chessstuff.Relation.ALL_KINDS);
+  }
 }
+
 
 function createPiece(color, kind) {
-    let piece = $("<p>" + SYMBOLS[color][kind] + "</p>");
-    piece.addClass("piece");
-    return piece;
+  let piece = document.createElement('p');
+  piece.textContent = SYMBOLS[color][kind];
+  piece.classList.add('piece');
+  return piece;
 }
 
-function refreshBoard(brd) {
-    $(".square").empty();
-    for (let piece of brd.squares.allPieces) {
-        $(`.square[x=${piece.square.name.symbol}][y=${piece.square.name.number}]`)
-        .append(createPiece(piece.color, piece.kind));
-    }
+
+
+function refreshBoard() {
+  let squares = document.querySelectorAll('.square');
+  for (let square of squares) {
+    square.innerHTML = '';
+  }
+
+  for (let piece of GLOBAL.board.squares.allPieces) {
+    let pieceElement = createPiece(piece.color, piece.kind);
+    let x = piece.square.name.symbol;
+    let y = piece.square.name.number;
+    let square = document.querySelector(`.square[x="${x}"][y="${y}"]`);
+    square.appendChild(pieceElement);
+  }
+}
+
+
+function showCover() {
+  let cover = document.querySelector('#cover');
+  cover.classList.remove('hided');
+}
+
+function hideCover() {
+  let cover = document.querySelector('#cover');
+  cover.classList.add('hided');
+}
+
+
+function showChoicesBox() {
+  let choicesBox = document.querySelector('#choicesBox');
+  choicesBox.classList.remove('hided');
+}
+
+function hideChoicesBox() {
+  let choicesBox = document.querySelector('#choicesBox');
+  choicesBox.classList.add('hided');
+  choicesBox.innerHTML = '';
 }
 
 
 function removeAdditionalInfo() {
-    for (let elem of ["#hider", "#choicesBox"]) {
-        $(elem).remove();
-    }
+  hideChoicesBox();
+  hideCover();
 }
 
-function hideBoard() {
-    let position = $(".square[x=a][y=8]").position();
-    let hider = $("<div></div>");
-    hider.attr("id", "hider");
-    hider.css("top", position.top);
-    hider.css("left", position.left);
-    $("body").append(hider);
+
+function fillChoicesBox() {
+  let choicesBox = document.querySelector('#choicesBox');
+  for (let kind of chessstuff.Piece.ALL_PAWN_TRANSFORM) {
+    let choicesItem = document.createElement('div');
+    choicesItem.classList.add('square', 'choicesItem');
+    choicesItem.addEventListener('click', function(e) {
+      e.stopPropagation();
+      removeAdditionalInfo();
+      response = GLOBAL.board.pawnTransformation(kind);
+      if (response.success) {
+        refreshBoard();
+      }
+    });
+    pieceElement = createPiece(GLOBAL.board.colors.current, kind);
+    choicesItem.appendChild(pieceElement);
+    choicesBox.appendChild(choicesItem);
+  }
 }
-
-function showTransformChoices(color) {
-    hideBoard();
-    let position = $(".square[x=a][y=8]").position();
-    let choicesBox = $("<div></div>");
-    choicesBox.addClass("row");
-    choicesBox.attr("id", "choicesBox");
-    choicesBox.css("top", position.top + 190);
-    choicesBox.css("left", position.left + 106);
-    for (let kind of ["queen", "rook", "bishop", "knight"]) {
-        let choicesItem = $("<div></div>");
-        choicesItem.addClass("square choicesItem");
-        choicesItem.on("click", function(e) {
-            e.stopPropagation();
-            removeAdditionalInfo();
-            response = board.pawnTransformation(kind);
-            if (response.success) {
-                refreshBoard(board);
-            }
-        })
-        choicesItem.append(createPiece(color, kind));
-        choicesBox.append(choicesItem);
-    }
-    $("body").append(choicesBox);
-}
-
-$("body").on("click", "#hider", function(e) {
-    removeAdditionalInfo();
-})
-
-$("body").on("click", ".choicesBox", function() {
-    removeAdditionalInfo();
-})
 
 
 function selectPiece(squareElem, squareName) {
-  let piece = board.squares[squareName].piece;
-  if (piece && piece.hasColor(board.colors.current)) {
-    $(squareElem).addClass([AIM, MARKED]);
+  let piece = GLOBAL.board.squares[squareName].piece;
+  if (piece && piece.hasColor(GLOBAL.board.colors.current)) {
+    squareElem.classList.add(AIM, MARKED);
     markingAbilitySquares(piece, true);
-    aimedSquare = squareName;
+    GLOBAL.aimedSquare = squareName;
   } else {
-    aimedSquare = null;
+    GLOBAL.aimedSquare = null;
   }
 }
 
 
 function movePiece(squareElem, squareName) {
-  let response = board.movePiece(aimedSquare, squareName);
+  let response = GLOBAL.board.movePiece(GLOBAL.aimedSquare, squareName);
   if (response.success) {
     if (response.transformation) {
-      showTransformChoices(board.colors.current);
+      showCover();
+      showChoicesBox();
+      fillChoicesBox();
+    } else {
+      refreshBoard();
     }
-    else {
-      refreshBoard(board);
-    }
-    aimedSquare = null;
+    GLOBAL.aimedSquare = null;
   } else {
-    selectPiece(this, squareName);
+    selectPiece(squareElem, squareName);
   }
 }
 
 
-$(".square").on("click", function() {
-    let newSquare = $(this).attr("x") + $(this).attr("y");
+function setSquaresEvents() {
+  let squares = document.querySelectorAll('.square');
+  for (let square of squares) {
+    square.addEventListener('click', function(e) {
+      let x = this.getAttributeNode('x').nodeValue;
+      let y = this.getAttributeNode('y').nodeValue;
+      let squareName = `${x}${y}`;
 
-    unMarkingSquares();
+      unMarkingSquares();
 
-    if (!aimedSquare) {
-      selectPiece(this, newSquare);
-    } else if (aimedSquare != newSquare) {
-      movePiece(this, newSquare);
-    }
-})
+      if (!GLOBAL.aimedSquare) {
+        selectPiece(this, squareName);
+      } else if (GLOBAL.aimedSquare != squareName) {
+        movePiece(this, squareName);
+      }
+    });
+  }
 
+  squares = document.querySelectorAll('.square:not(.aim)');
+  for (let square of squares) {
+    square.addEventListener('mouseenter', function(e) {
+      if (GLOBAL.aimedSquare) return;
 
-$(".square:not(.aim)")
-.mouseenter(function(e) {
-  if (aimedSquare) return;
+      let x = this.getAttributeNode('x').nodeValue;
+      let y = this.getAttributeNode('y').nodeValue;
+      let squareName = `${x}${y}`;
+      let piece = GLOBAL.board.squares[squareName].piece;
+      if (!piece) return;
 
-  let squareName = $(this).attr("x") + $(this).attr("y");
-  let piece = board.squares[squareName].piece;
-  if (!piece) return;
+      markingAbilitySquares(piece);
+    });
 
-  markingAbilitySquares(piece);
-})
-.mouseleave(function(e) {
-  if (aimedSquare) return;
-  unMarkingSquares();
-});
+    square.addEventListener('mouseleave', function(e) {
+      if (GLOBAL.aimedSquare) return;
+      unMarkingSquares();
+    });
+  }
+}
 
 
 function setActionEvents(action) {
   let input = document.getElementById(`actions-${action}`);
   input.addEventListener('change', function(e) {
-    globalActions[action] = this.checked;
+    GLOBAL.actions[action] = this.checked;
   });
-  globalActions[action] = input.checked;
+  GLOBAL.actions[action] = input.checked;
 }
 
 
@@ -192,6 +224,13 @@ function setActionsEvents() {
 document.addEventListener('DOMContentLoaded', function(e) {
   setActionsEvents();
 
-  board = new chessstuff.Board(chessstuff.Board.INITIAL_FEN);
-  refreshBoard(board);
+  setSquaresEvents();
+
+  let cover = document.querySelector('#cover');
+  cover.addEventListener('click', function(e) {
+      removeAdditionalInfo();
+  });
+
+  GLOBAL.board = new chessstuff.Board(chessstuff.Board.INITIAL_FEN);
+  refreshBoard();
 });
